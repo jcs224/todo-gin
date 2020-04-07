c188469ec2b4e093c94b459e01ea10badbd7420a
Joe Sweeney <joe.sweeney224@gmail.com>
2020-04-06 22:18:32 -0600

    A bit more setup

--- main.go
+++ main.go
@@ -4,9 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type Todo struct {
+	gorm.Model
+	Text      string
+	Completed bool
+}
+
 func main() {
+	db, err := gorm.Open("mysql", "root:password@/todo_gin?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Todo{})
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 
